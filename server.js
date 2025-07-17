8eb86fe75f47003597fa1af01430b7f4521c629e
IcedMar <mbanimarine@gmail.com>
2025-07-17 11:03:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -517,6 +517,10 @@ app.post('/stk-callback', stkCallbackRateLimiter, async (req, res) => {
     res.json({ ResultCode: 0, ResultDesc: 'Callback received and payment status recorded by DaimaPay server.' });
 });
 
+app.get("/ping", (req, res) => {
+  res.status(200).send("pong");
+});
+
 // --- Start the Server ---
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
